2008b065eaabccb0974ac3540bb7e4163f2738c8
f1shift <formyfarrari@gmail.com>
2025-07-21 23:31:17 +0900

    firehose追加

--- infra/aws-terraform/modules/stack01/firehose.tf
+++ infra/aws-terraform/modules/stack01/firehose.tf
@@ -23,35 +23,51 @@ resource "aws_iam_policy" "firehose_policy" {
     Version = "2012-10-17"
     Statement = [
       {
+        Effect = "Allow",
+        Action = [
+          "glue:GetDatabase",
+          "glue:GetPartitions",
+          "glue:GetTable",
+          "glue:UpdateTable",
+          "glue:CreateTable"
+        ],
+        Resource = [
+          "arn:aws:glue:*:${data.aws_caller_identity.current.account_id}:catalog",
+          "arn:aws:glue:*:${data.aws_caller_identity.current.account_id}:database/${var.locals_env.sagemaker_unified_studio.default_glue_database}*",
+          # "arn:aws:glue:*:${data.aws_caller_identity.current.account_id}:catalog/${var.locals_env.sagemaker_unified_studio.default_glue_database}*",
+          aws_glue_catalog_table.traffic_data.arn,
+        ]
+      },
+      {
+        Effect = "Allow"
         Action = [
           "s3:ListBucket",
         ]
-        Effect   = "Allow"
         Resource = "arn:aws:s3:::${var.locals_env.sagemaker_unified_studio.default_glue_s3_bucket}"
       },
       {
+        Effect = "Allow"
         Action = [
           "s3:PutObject",
           "s3:GetObject",
           "s3:DeleteObject",
           "s3:PutObjectAcl"
         ]
-        Effect   = "Allow"
         Resource = "arn:aws:s3:::${var.locals_env.sagemaker_unified_studio.default_glue_s3_bucket}/*"
       },
       {
+        Effect = "Allow"
         Action = [
           "logs:CreateLogGroup",
         ]
-        Effect   = "Allow"
         Resource = "arn:aws:logs:*:${data.aws_caller_identity.current.account_id}:log-group:*"
       },
       {
+        Effect = "Allow"
         Action = [
           "logs:CreateLogStream",
           "logs:PutLogEvents",
         ]
-        Effect   = "Allow"
         Resource = "arn:aws:logs:*:${data.aws_caller_identity.current.account_id}:log-group:*:log-stream:*"
       },
     ]
@@ -64,6 +80,16 @@ resource "aws_iam_policy_attachment" "firehost_policy_attachment" {
   policy_arn = aws_iam_policy.firehose_policy.arn
 }
 
+resource "aws_lakeformation_permissions" "database_permissions" {
+  principal   = aws_iam_role.firehose_role.arn
+  permissions = ["DESCRIBE"]
+
+  database {
+    name       = aws_glue_catalog_table.traffic_data.database_name
+    catalog_id = data.aws_caller_identity.current.account_id
+  }
+}
+
 resource "aws_lakeformation_permissions" "traffic_data_table_permissions" {
   principal   = aws_iam_role.firehose_role.arn
   permissions = ["SELECT", "INSERT", "DELETE", "DESCRIBE"]
@@ -74,7 +100,12 @@ resource "aws_lakeformation_permissions" "traffic_data_table_permissions" {
   }
 }
 
-resource "aws_kinesis_firehose_delivery_stream" "test_stream" {
+resource "aws_kinesis_firehose_delivery_stream" "firehose1" {
+  depends_on = [
+    aws_glue_catalog_table.traffic_data,
+    aws_lakeformation_permissions.database_permissions,
+    aws_lakeformation_permissions.traffic_data_table_permissions,
+  ]
   name        = "${var.locals_env.resource_prefix}-firehose"
   destination = "iceberg"
 
